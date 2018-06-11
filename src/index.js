7d59a28015e1aeda45939eadd67a8047dca7225c
monisahmad <monis.ahmad42@gmail.com>
2018-06-12 04:58:31 +0530

    WIP: Monis-Segment-2_update1

--- src/index.js
+++ src/index.js
@@ -64,10 +64,12 @@ class Queue {
 
 // 7 (*)
 // Convert the function below to ES6 class (*)
-function Person(firstName, lastName, dateOfBirth) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.dateOfBirth = dateOfBirth;
+class Person {
+  constructor(firstName, lastName, dateOfBirth) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+  }
 }
 
 Person.prototype.addDobDigits = function addDobDigits() {
